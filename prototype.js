ae2feafc72926f0f154f708a224f07d6de2ae1ca
asolberg <asolberg@roomandboard.com>
2015-10-20 14:25:40 -0500

    added prototype

--- prototype.js
+++ prototype.js
@@ -1,3 +1,4 @@
+// Just a unique id counter.
 var VEHICLE_ID = 0;
 
 /** The prototype object used for all vehicles. */
@@ -8,8 +9,10 @@ var Vehicle = {
 };
 
 /** 
-  * The a car object that inherits the vehicle prototype
-  * and the id and model properties defined in the property definition object literal.
+  * A car object that inherits the vehicle prototype and the id and model 
+  * properties defined in the property definition object literal.
+  * Protype chain for 'car':
+  * car ---> Vehicle ---> Object.prototype ---> null
   */
 var car = Object.create(Vehicle, {
 
@@ -29,3 +32,29 @@ var car = Object.create(Vehicle, {
 console.log(car);               // Object {id: 1, model: "Ford"}
 console.log(car.id);            // 1
 console.log(car.getModel());    // The model of this vehicle is Ford
+
+/** 
+  * A car object that inherits the vehicle prototype and the id and model 
+  * properties defined in the property definition object literal.
+  * Protype chain for 'car':
+  * suv ---> car ---> Vehicle ---> Object.prototype ---> null
+  */
+var suv = Object.create(car, {
+
+    'wheel_base': {
+        value: '120 inches',
+        // writable:false, configurable:false by default
+        enumerable: true
+    },
+
+    'getWheelBase': {
+        value: function () { return this.wheel_base; },
+        enumerable: true
+    }
+
+});
+
+console.log(suv);                   // Object {wheel_base: "120 inches"}
+console.log(suv.id);                // 1
+console.log(suv.getModel());        // The model of this vehicle is Ford
+console.log(suv.getWheelBase());    // 120 inches
